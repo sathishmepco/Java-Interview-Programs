ccd1898aa5feab96179e836be762f18927aee92b
sathishmepco <sathishmepco@gmail.com>
2019-09-24 09:52:44 +0530

    Pattern Question

---
+++ InterviewPrograms/src/com/java/patterns/Pattern3.java
@@ -0,0 +1,47 @@
+package com.java.patterns;
+
+import java.util.Scanner;
+
+/*
+ * 	Write a Java Program to print the following Pattern
+
+	6 5 4 3 2 1
+	6 5 4 3 2 
+	6 5 4 3 
+	6 5 4
+	6 5
+	6
+ */
+public class Pattern3 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter the number of rows to print the pattern :: ");
+		int N = Integer.parseInt(scanner.nextLine().trim());
+		
+		//logic to print the pattern
+		for(int i=N; i>=1; i--){
+			for(int j=1, value=N; j<=i; j++, value--)
+				System.out.print(value+" ");
+			System.out.println();
+		}
+		
+		scanner.close();
+	}
+}
+
+/*
+	OUTPUT
+	
+	Enter the number of rows to print the pattern :: 10
+	10 9 8 7 6 5 4 3 2 1 
+	10 9 8 7 6 5 4 3 2 
+	10 9 8 7 6 5 4 3 
+	10 9 8 7 6 5 4 
+	10 9 8 7 6 5 
+	10 9 8 7 6 
+	10 9 8 7 
+	10 9 8 
+	10 9 
+	10 
+
+*/
