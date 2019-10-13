713efb2d6ddca4df73f9a79a24560afa7e3d3804
sathishmepco <sathishmepco@gmail.com>
2019-10-13 23:23:08 +0530

    Even Number Series

---
+++ InterviewPrograms/src/com/techgig/series/EvenNumberSeries.java
@@ -0,0 +1,37 @@
+package com.techgig.series;
+
+import java.util.Scanner;
+
+/*
+ * Print the EVEN number series
+ * 
+ * 0 2 4 6 8 10 12 14 ....
+ * 
+ * In the for loop, either use i++, check i is even and print
+ * Else in the for loop use i+=2 then print i value
+ * i starts with value 0
+ */
+public class EvenNumberSeries {
+	
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter the N(end value) value for even number series : ");
+		int N = Integer.parseInt(scanner.nextLine().trim());
+		
+		for(int i=0;i<=N;i+=2)
+			System.out.print(i+" ");
+		
+		scanner.close();
+	}
+}
+
+/*
+	OUTPUT
+	
+	Enter the N(end value) value for even number series : 25
+	0 2 4 6 8 10 12 14 16 18 20 22 24
+	
+	Enter the N(end value) value for even number series : 14
+	0 2 4 6 8 10 12 14
+	 
+*/
