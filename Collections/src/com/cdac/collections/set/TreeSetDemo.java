0f4d51ff14a90721d2ac37e1dcaa749780257222
sathishmepco <sathishmepco@gmail.com>
2019-11-11 23:27:27 +0530

    Collectinos - TreeSEt

--- Collections/src/com/cdac/collections/set/TreeSetDemo.java
+++ Collections/src/com/cdac/collections/set/TreeSetDemo.java
@@ -11,20 +11,23 @@ public class TreeSetDemo {
 		set.add("India");
 		set.add("Pakistan");
 		set.add("China");
+		set.add("US");
+		set.add("Australia");
 		
 		//SEARCH OR CONTAINS
 		set.contains("India");
 		
 		//READ OR ITERATE
-		String minElement = set.first();	//lowest element
-		String maxElement = set.last();		//highest element
-		String nextMax = set.higher("India");	//strictly highest than India
-		String nextMin = set.lower("India");		//strictly lower than India
-
+		//lowest element - first element in the set
+		String minElement = set.first();	
+		//highest element - last element in the set
+		String maxElement = set.last();		
+		
 		//Iterate all elements using Iterator
 		Iterator<String> iterator = set.iterator();
 		while (iterator.hasNext()){
-			iterator.next();
+			System.out.println(iterator.next()); 
+			
 		}
 		
 		//SIZE AND EMPTY STATE
@@ -32,7 +35,32 @@ public class TreeSetDemo {
 		boolean emptyState = set.isEmpty();
 		
 		//REMOE THE GIVEN ELEMENT
-		boolean isRemoved = set.remove("China");
+//		boolean isRemoved = set.remove("China");
+		
+		System.out.println(set);
+		
+		//READS THE FIRST ELEMENT AND REMOVES IT
+		String pollFirst = set.pollFirst();
+		System.out.println("Poll First [First value] : "+pollFirst);
+		
+		//READS AND REMOVES THE LAST ELEMENT
+		String pollLast = set.pollLast();
+		System.out.println("Poll Last [Last value] : "+pollLast);
+		
+		//strictly highest than India
+		String nextMax = set.higher("India");
+		System.out.println("Highest value {India} is : "+nextMax);
+		
+		//strictly lower than India
+		String nextMin = set.lower("India");
+		System.out.println("Lowest value {India} is : "+nextMin);
+
+		//returns the next maximum to "High", the output is "India"
+		String ceilingValue = set.ceiling("High");
+		System.out.println("Ceiling value {High} is : "+ceilingValue);
+		
+		String floorValue = set.floor("China");
+		System.out.println("Floor value {China} is : "+floorValue);
 		
 		//REMVOE ALL THE ELEMENTS
 		set.clear();
