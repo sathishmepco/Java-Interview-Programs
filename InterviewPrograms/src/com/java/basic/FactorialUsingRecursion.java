d4bb6b1931030523f671cc3471d460c66c81b89a
sathishmepco <sathishmepco@gmail.com>
2019-12-17 21:07:20 +0530

    Factorial using Recursion

---
+++ InterviewPrograms/src/com/java/basic/FactorialUsingRecursion.java
@@ -0,0 +1,56 @@
+package com.java.basic;
+
+import java.util.Scanner;
+
+/*
+ * 	Factorial Using Recursion
+ * 
+ * In mathematics, the factorial of a positive integer n, 
+ * denoted by n!, is the product of all positive integers 
+ * less than or equal to n:
+ * 
+ *  n! can be written as 
+ *  n! = 1 * 2 * 3 * .... * (n-1) * n;
+ *  
+ *  n! can also be written as
+ *  n! = n * (n-1)!
+ *  This approach helps to calculate using Recursion
+ *  
+ *  5! = 5 * 4!
+ *  4! = 4 * 3!
+ *  3! = 3 * 2!
+ *  2! = 2 * 1!
+ *  1! = 1
+ *  
+ */
+public class FactorialUsingRecursion {
+	
+	//using recursion calculate factorial
+	public static int factorial(int n){
+		if(n == 1)
+			return 1;
+		//n * (n-1)!
+		return n * factorial(n-1);
+	}
+	
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter the any number : ");
+		int N = Integer.parseInt(scanner.nextLine().trim());
+		
+		int result = factorial(N);
+		
+		String output = String.format("Factorial of %d is %d", N,result);
+		System.out.println(output);
+		scanner.close();
+	}
+}
+/*
+	OUTPUT
+	
+	Enter the any number : 5
+	Factorial of 5 is 120
+	
+	Enter the any number : 10
+	Factorial of 10 is 3628800
+*/
