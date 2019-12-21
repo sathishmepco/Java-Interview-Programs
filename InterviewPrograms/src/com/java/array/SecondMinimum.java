79db66e85be129320f1616991bd390c2c8c766c1
sathishmepco <sathishmepco@gmail.com>
2019-12-21 16:21:37 +0530

    Array Program - Second Minimum

--- InterviewPrograms/src/com/java/array/SecondMinimum.java
+++ InterviewPrograms/src/com/java/array/SecondMinimum.java
@@ -2,32 +2,49 @@ package com.java.array;
 
 import java.util.Arrays;
 
-//This code will find the Second Minimum in the given array
 /*
-* say Given Array is {10,32,34,54,19,29,38,45}
+* Find the Second Minimum in the Given Array
+* 
+* array {10,32,34,54,19,29,38,45}
 * minimum is 10, second minimum is 19
 * 
-* say Given Array is {10,20,30,40,50,60,70,80}
+* array is {10,20,30,40,50,60,70,80}
 * minimum is 10, second minimum is 20
+* 
+* The solution is, declare two variables
+* one variable to store minimum in the array
+* another variable to store second minimum
+* 
 */
 public class SecondMinimum {
 	public static void main(String[] args) {
 		
 		int array[] = {10,32,34,54,19,29,38,45};
-		
+	
+		// assume 0th index value is minimum
 		int min = array[0];
+		// assume 1st index value is 2nd minimum
 		int secondMin = array[1];
 		
+		//if 1st index value is minimum than 0th index value
+		//then swap the min and secondMin variable
 		if(min > secondMin){
 			min = array[1];
 			secondMin = array[0];
 		}
 		
+		//iterate from 2nd index
 		for(int i=2;i<array.length;i++){
 			if(array[i] < min){
+				//if ith index is less than minimum 
+				//then update min and secondMin
 				secondMin = min;
 				min = array[i];
 			}else if(array[i] < secondMin){
+				//if ith index is not less than min
+				//still need to check with secondmin
+				//if ith index less than second min
+				//then update only second min
 				secondMin = array[i];
 			}
 		}
@@ -39,10 +56,11 @@ public class SecondMinimum {
 }
 /*
 
-OUTPUT
+	OUTPUT
 
-Given array is : [10, 32, 34, 54, 19, 29, 38, 45]
-Minimum value in the array is : 10
-Second minimum value in the array is : 19
+	Given array is : 
+	[10, 32, 34, 54, 19, 29, 38, 45]
+	Minimum value in the array is : 10
+	Second minimum value in the array is : 19
   
 */
