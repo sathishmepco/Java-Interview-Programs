3de830c8a898173bc40f573499d0f73b2b3ddfd0
sathishmepco <sathishmepco@gmail.com>
2019-12-21 18:36:26 +0530

    Find Unique element in the Array

--- InterviewPrograms/src/com/java/array/FindUniqueElt.java
+++ InterviewPrograms/src/com/java/array/FindUniqueElt.java
@@ -1,16 +1,53 @@
 package com.java.array;
 
 /*
- In the given array, only one element is not repeated 
- all other elements are repeated exactly 2 times. 
- Find the element which occured only one time. 
+ * Find the only one unique Element in the Array
+ * 
+ * In the given array only one element is 
+ * occurred one time, all the other elements are
+ * repeated exactly two times.
+ * Find the element which occurred only one time
+ * 
+ * array is { 1, 2, 3, 4, 5, 6, 1, 2, 3, 4, 5};
+ * 
+ * 6 is repeated only one time
+ * 1, 2, 3, 4, 5 all repeated two times
+ * 
+ *  Write the program to calculate 6
+ * 
+ * Solution
+ * 
+ * use ^ (XOR) operator
+ * apply ^ (XOR) operator to all the values 
+ * in the array.
+ * when XOR apply between same value result is 0
+ * 
+ * Finally we will the output as the value
+ * which occurred only one time
  */
 public class FindUniqueElt {
 	public static void main(String[] args) {
-		int array[] = { 1, 2, 3, 4, 5, 6, 1, 2, 3, 4, 5};
+		//int array[] = { 1, 2, 3, 4, 5, 6, 1, 2, 3, 4, 5};
+		int array[] = {5, 10, 33, 42, 5, 42, 10};
 		int output = array[0];
 		for(int i=1;i<array.length;i++)
 			output ^= array[i];
-		System.out.println(output);
+		
+		System.out.println("Given array is :");
+		for(int i : array){
+			System.out.print(i+" ");
+		}
+		System.out.println("\nUnique element in the array is :"+output);
 	}
 }
+/*
+	OUTPUT
+	
+	Given array is :
+	1 2 3 4 5 6 1 2 3 4 5 
+ 	Unique element in the array is : 6
+ 	
+ 	Given array is :
+	 
+ 	Unique element in the array is : 33
+*/
