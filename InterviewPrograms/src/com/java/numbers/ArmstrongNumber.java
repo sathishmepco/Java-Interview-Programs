e5021993d1309c4c1db93363f53c84f06ec63349
sathishmepco <sathishmepco@gmail.com>
2019-12-25 21:52:29 +0530

    Armstrong Number

--- InterviewPrograms/src/com/java/numbers/ArmstrongNumber.java
+++ InterviewPrograms/src/com/java/numbers/ArmstrongNumber.java
@@ -3,13 +3,16 @@ package com.java.numbers;
 import java.util.Scanner;
 
 /*
- * Armstrong Number
- * A positive number is called armstrong number if it is equal to the 
+ * 	Armstrong Number
+ * 
+ * A positive number is called Armstrong number 
+ * if it is equal to the 
  * sum of cubes of its digits 
  * for example 0, 1, 153, 370, 371, 407 etc.
  * 
  * 153 = 1^3 + 5^3 + 3^3 = 1 + 125 + 27 = 153
- * 370 = 3^3 + 7^3 + 0^3 = 27 + 343 + 0 = 370 
+ * 370 = 3^3 + 7^3 + 0^3 = 27 + 343 + 0 = 370
+ * 371 = 3^3 + 7^3 + 1^3 = 27 + 343 + 1 = 371
  */
 public class ArmstrongNumber {
 	
@@ -19,9 +22,9 @@ public class ArmstrongNumber {
 		int num = Integer.parseInt(scanner.nextLine().trim());
 
 		if( checkArmstrongNumber(num) == true )
-			System.out.println("The given number "+num+" is a Armsrong Number.");
+			System.out.println("Given number "+num+" is a Armstrong Number.");
 		else
-			System.out.println("The given number "+num+" is NOT a Armsrong Number.");
+			System.out.println("Given number "+num+" is NOT a Armstrong Number.");
 		scanner.close();
 	}
 	
@@ -47,12 +50,12 @@ public class ArmstrongNumber {
 	OUTPUT
 
 	Enter any positive number :: 153
-	The given number 153 is a Armsrong Number.
+	The given number 153 is a Armstrong Number.
 	
 	Enter any positive number :: 370
-	The given number 153 is a Armsrong Number.
+	The given number 153 is a Armstrong Number.
 
 	Enter any positive number :: 500
-	The given number 500 is NOT a Armsrong Number.
+	The given number 500 is NOT a Armstrong Number.
 	
 */
