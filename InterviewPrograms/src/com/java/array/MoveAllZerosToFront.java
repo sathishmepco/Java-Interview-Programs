2849c7cc6eb87f157136b0b5a66469b2db0aee37
sathishmepco <sathishmepco@gmail.com>
2019-12-30 23:17:54 +0530

    Move all zeros to start of the array

---
+++ InterviewPrograms/src/com/java/array/MoveAllZerosToFront.java
@@ -0,0 +1,40 @@
+package com.java.array;
+
+/*
+ * Move All the Zeros to End of the Array
+ * 
+ * Assume the given array contains some zero
+ * in some index positions.
+ * 
+ * Move all the zero to end of the Array
+ * 
+ * array[] = {1, 0, 4, 3, 0, 0, 2, 0, 1, 0}
+ */
+public class MoveAllZerosToFront {
+	public static void main(String[] args) {
+		int array[] = {1, 0, 4, 3, 0, 0, 2, 0, 1, 0};
+		System.out.println("The Given Array is :: ");
+		for(int v : array)
+			System.out.print(v+" ");
+		
+		int count = 0;
+		for(int i=0;i<array.length;i++)
+			if(array[i] != 0)
+				array[count++] = array[i];
+		
+		while(count < array.length)
+			array[count++] = 0;
+		
+		System.out.println("\nAfter moving all zeros to end of array :: ");
+		for(int v : array)
+			System.out.print(v+" ");
+		
+	}
+}
+/*
+	OUTPUT
+	The Given Array is :: 
+	1 0 4 3 0 0 2 0 1 0 
+	After moving all zeros to end of array :: 
+	1 4 3 2 1 0 0 0 0 0 
+*/
