7061ab609f0bbe79fdec68b580c1e6fa51d27f49
sathishmepco <sathishmepco@gmail.com>
2020-01-06 23:13:57 +0530

    Octal to Decimal Conversion

---
+++ InterviewPrograms/src/com/java/convertor/OctalToDecimal.java
@@ -0,0 +1,31 @@
+package com.java.convertor;
+
+/*
+ * Octal To Decimal Conversion
+ * 
+ * Octal Number System
+ * The octal numeral system, or oct for short, 
+ * is the base-8 number system, and uses 
+ * the digits 0 to 7. Octal numerals can be 
+ * made from binary numerals by grouping 
+ * consecutive binary digits into groups 
+ * of three (starting from the right). 
+ * In the octal system each place 
+ * is a power of eight.
+ * 
+ * Decimal Number System
+ * Decimal number system, in mathematics, 
+ * positional numeral system employing 10 as 
+ * the base and requiring 10 different numerals, 
+ * the digits 0, 1, 2, 3, 4, 5, 6, 7, 8, 9. 
+ * It also requires a dot (decimal point) to 
+ * represent decimal fractions.
+ */
+public class OctalToDecimal {
+	public static void main(String[] args) {
+		
+	}
+}
+/*
+	OUTPUT
+*/
