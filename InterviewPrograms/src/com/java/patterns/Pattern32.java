513fa1107f892e214175a3647a6bb87e2b1baa3b
sathishmepco@gmail.com <sathishmepco@gmail.com>
2020-05-10 13:58:54 +0530

    Pattern 32 - Techgig

---
+++ InterviewPrograms/src/com/java/patterns/Pattern32.java
@@ -0,0 +1,39 @@
+package com.java.patterns;
+import java.util.Scanner;
+/*
+Write the program to print the following pattern
+        A
+      B B
+    C C C
+  D D D D
+E E E E E
+*/
+public class Pattern32 {
+	public static void main(String args[] ) throws Exception {
+		Scanner scanner = new Scanner(System.in);
+		int N = Integer.parseInt(scanner.nextLine());
+		String s = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+		for(int i=0;i<N;i++){
+			for(int j = 0;j<N-i-1;j++)
+				System.out.print("  ");
+			for(int j=0;j<=i;j++)
+				if(j == 0)
+					System.out.print(s.charAt(i));
+				else
+					System.out.print(" "+s.charAt(i));
+			if(i != N-1)
+				System.out.println();
+		}
+		scanner.close();
+	}
+}
+/*
+Input
+5
+Output
+        A
+      B B
+    C C C
+  D D D D
+E E E E E
+*/
