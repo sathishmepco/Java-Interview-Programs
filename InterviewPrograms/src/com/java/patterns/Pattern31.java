7c8c77e31944126468832b3226da939c995ff814
sathishmepco@gmail.com <sathishmepco@gmail.com>
2020-05-10 13:57:50 +0530

    Pattern 31 - Techgig

---
+++ InterviewPrograms/src/com/java/patterns/Pattern31.java
@@ -0,0 +1,39 @@
+package com.java.patterns;
+import java.util.Scanner;
+/*
+Write the program to print the following pattern
+        A
+      A B
+    A B C
+  A B C D
+A B C D E
+*/
+public class Pattern31 {
+	public static void main(String args[] ) throws Exception {
+		Scanner scanner = new Scanner(System.in);
+		int N = Integer.parseInt(scanner.nextLine());
+		String s = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+		for(int i=0;i<N;i++){
+			for(int j = 0;j<N-i-1;j++)
+				System.out.print("  ");
+			for(int j=0;j<=i;j++)
+				if(j == 0)
+					System.out.print(s.charAt(j));
+				else
+					System.out.print(" "+s.charAt(j));
+			if(i != N-1)
+				System.out.println();
+		}
+		scanner.close();
+	}
+}
+/*
+Input
+5
+Output
+        A
+      A B
+    A B C
+  A B C D
+A B C D E
+*/
