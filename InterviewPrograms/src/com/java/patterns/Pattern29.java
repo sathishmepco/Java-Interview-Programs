ed4d7d45bd2973e8bc8387263705e620afe598d5
sathishmepco@gmail.com <sathishmepco@gmail.com>
2020-05-10 13:55:22 +0530

    Pattern 29 - Techgig

---
+++ InterviewPrograms/src/com/java/patterns/Pattern29.java
@@ -0,0 +1,38 @@
+package com.java.patterns;
+import java.util.Scanner;
+/*
+Write the program to print the following pattern
+5 5 5 5 5
+  4 4 4 4
+    3 3 3
+      2 2
+        1
+*/
+public class Pattern29 {
+	public static void main(String args[] ) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+        int N = Integer.parseInt(scanner.nextLine());
+        for(int i=0;i<N;i++){
+            for(int j = 0;j<i;j++)
+				System.out.print("  ");
+            for(int j=i;j<N;j++)
+                if(j == i)
+                    System.out.print(N-i);
+                else
+                    System.out.print(" "+(N-i));
+            if(i != N-1)
+                System.out.println();
+        }
+        scanner.close();
+   }
+}
+/*
+Input
+5
+Output
+5 5 5 5 5
+  4 4 4 4
+    3 3 3
+      2 2
+        1
+*/
