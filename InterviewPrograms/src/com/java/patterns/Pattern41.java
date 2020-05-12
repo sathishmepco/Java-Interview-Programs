5b5dac47ed31ce3cc323808a4ebfb271429e554f
sathishmepco@gmail.com <sathishmepco@gmail.com>
2020-05-12 23:50:37 +0530

    Pattern 41 - Techgig

---
+++ InterviewPrograms/src/com/java/patterns/Pattern41.java
@@ -0,0 +1,37 @@
+package com.java.patterns;
+import java.util.Scanner;
+/*
+Write the program to print the following pattern
+A A A A A
+B B B B
+C C C
+D D
+E
+*/
+public class Pattern41 {
+	public static void main(String args[] ) throws Exception {
+		Scanner scanner = new Scanner(System.in);
+		int N = Integer.parseInt(scanner.nextLine());
+		String s = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+		for(int i=0;i<N;i++){
+			for(int j=0;j<N-i;j++)
+				if(j == 0)
+					System.out.print(s.charAt(i));
+				else
+					System.out.print(" "+s.charAt(i));
+			if(i != N-1)
+				System.out.println();
+		}
+		scanner.close();
+	}
+}
+/*
+Input
+5
+Output
+A A A A A
+B B B B
+C C C
+D D
+E
+*/
