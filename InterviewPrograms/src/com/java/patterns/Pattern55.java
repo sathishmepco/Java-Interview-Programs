7fe567b0f1cab42f997e727f89e01624556cd9cb
sathishmepco@gmail.com <sathishmepco@gmail.com>
2020-05-17 20:32:04 +0530

    Pattern 55 - TechGig

---
+++ InterviewPrograms/src/com/java/patterns/Pattern55.java
@@ -0,0 +1,36 @@
+package com.java.patterns;
+import java.util.Scanner;
+/*
+Write the program to print the following pattern
+1
+2 2
+3 3 3
+4 4 4 4
+5 5 5 5 5
+*/
+public class Pattern55 {
+	public static void main(String args[] ) throws Exception {
+		Scanner scanner = new Scanner(System.in);
+		int N = Integer.parseInt(scanner.nextLine());
+		for(int i=0;i<N;i++){
+			for(int j=0;j<=i;j++)
+				if(j == 0)
+					System.out.print(i+1);
+				else
+					System.out.print(" "+(i+1));
+			if(i != N-1)
+				System.out.println();
+		}
+		scanner.close();
+	}
+}
+/*
+Input
+5
+Output
+1
+2 2
+3 3 3
+4 4 4 4
+5 5 5 5 5
+*/
