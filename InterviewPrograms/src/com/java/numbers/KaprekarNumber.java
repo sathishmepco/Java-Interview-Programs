3648ecbed44ebf17fc5d832ca1f1d52dea30632a
sathishmepco@gmail.com <sathishmepco@gmail.com>
2020-06-27 00:13:40 +0530

    Kaprekar Number

--- InterviewPrograms/src/com/java/numbers/KaprekarNumber.java
+++ InterviewPrograms/src/com/java/numbers/KaprekarNumber.java
@@ -29,11 +29,33 @@ public class KaprekarNumber {
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("Enter any positive number :: ");
 		int num = Integer.parseInt(scanner.nextLine().trim());
-		
+		if(isKaprekarNumber(num) == true )
+			System.out.println("Given number "+num+" is a Kaprekar Number.");
+		else
+			System.out.println("Given number "+num+" is NOT a Kaprekar Number.");
 		scanner.close();
 	}
 	
 	public static boolean isKaprekarNumber(int num){
-		
+		long square = num * num;
+		String str = Long.toString(square);
+		for(int i=1;i<str.length();i++){
+			String part1 = str.substring(0,i);
+			String part2 = str.substring(i,str.length());
+			long l1 = Long.valueOf(part1);
+			long l2 = Long.valueOf(part2);
+			if(l1 > 0 && l2 > 0 && l1+l2 == num)
+				return true;
+		}
+		return false;
 	}
 }
+/*
+	OUTPUT
+	
+	Enter any positive number :: 45
+	Given number 45 is a Kaprekar Number.
+	
+	Enter any positive number :: 20
+	Given number 20 is NOT a Kaprekar Number.
+*/
