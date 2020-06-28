d6013155f8062cc5d65d34669e8a5e7ace30a501
sathishmepco@gmail.com <sathishmepco@gmail.com>
2020-06-28 23:44:15 +0530

    Add two numbers without using arithmetic operator

---
+++ InterviewPrograms/src/com/java/basic/AddWithoutPlus.java
@@ -0,0 +1,30 @@
+package com.java.basic;
+
+/*
+ * Add two numbers a,b without using + operator
+ * 
+ * Use bitwise operators instead of using + operator
+ * bitwise and = &
+ * xor operator = ^
+ * shift operator = <<
+ *  
+ */
+public class AddWithoutPlus {
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 30;
+		while(b != 0){
+			int carry = a & b;
+			a = a ^ b;
+			b = carry << 1;
+		}
+		System.out.println("Sum of a and b is : "+a);
+	}
+}
+/*
+	INPUT
+	a = 10
+	b = 30
+	OUTPUT
+	Sum of a and b is : 40
+*/
