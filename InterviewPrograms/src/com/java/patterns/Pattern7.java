f8e8d62f65d5e3fee6b867573e3862192b5e2ab2
sathishmepco@gmail.com <sathishmepco@gmail.com>
2020-07-03 23:20:35 +0530

    Pattern 7

--- InterviewPrograms/src/com/java/patterns/Pattern7.java
+++ InterviewPrograms/src/com/java/patterns/Pattern7.java
@@ -5,15 +5,15 @@ import java.util.Scanner;
 /*
  * 	Write a Java Program to print the following Pattern
 	1 2 3 4 5 6 
-	2 3 4 5 6 
-	3 4 5 6 
-	4 5 6 
-	5 6 
-	6 
-	5 6 
-	4 5 6 
-	3 4 5 6 
-	2 3 4 5 6 
+	  2 3 4 5 6 
+	    3 4 5 6 
+	      4 5 6 
+	        5 6 
+	          6 
+	        5 6 
+	      4 5 6 
+	    3 4 5 6 
+	  2 3 4 5 6 
 	1 2 3 4 5 6 
  */
 public class Pattern7 {
@@ -23,11 +23,15 @@ public class Pattern7 {
 		int N = Integer.parseInt(scanner.nextLine().trim());
 		
 		for(int i=1;i<=N;i++){
+			for(int k=1;k<i;k++)
+				System.out.print("  ");
 			for(int j=i;j<=N;j++)
 				System.out.print(j+" ");
 			System.out.println();
 		}
 		for(int i=N-1; i>=1; i--){
+			for(int k=1;k<i;k++)
+				System.out.print("  ");
 			for(int j=i;j<=N;j++)
 				System.out.print(j+" ");
 			System.out.println();
@@ -41,22 +45,22 @@ public class Pattern7 {
 	
 	Enter the number of rows to print the pattern :: 10
 	1 2 3 4 5 6 7 8 9 10 
-	2 3 4 5 6 7 8 9 10 
-	3 4 5 6 7 8 9 10 
-	4 5 6 7 8 9 10 
-	5 6 7 8 9 10 
-	6 7 8 9 10 
-	7 8 9 10 
-	8 9 10 
-	9 10 
-	10 
-	9 10 
-	8 9 10 
-	7 8 9 10 
-	6 7 8 9 10 
-	5 6 7 8 9 10 
-	4 5 6 7 8 9 10 
-	3 4 5 6 7 8 9 10 
-	2 3 4 5 6 7 8 9 10 
-	1 2 3 4 5 6 7 8 9 10 
+	  2 3 4 5 6 7 8 9 10 
+	    3 4 5 6 7 8 9 10 
+	      4 5 6 7 8 9 10 
+	        5 6 7 8 9 10 
+	          6 7 8 9 10 
+	            7 8 9 10 
+	              8 9 10 
+	                9 10 
+	                  10 
+	                9 10 
+	              8 9 10 
+	            7 8 9 10 
+	          6 7 8 9 10 
+	        5 6 7 8 9 10 
+	      4 5 6 7 8 9 10 
+	    3 4 5 6 7 8 9 10 
+	  2 3 4 5 6 7 8 9 10 
+	1 2 3 4 5 6 7 8 9 10  
 */
