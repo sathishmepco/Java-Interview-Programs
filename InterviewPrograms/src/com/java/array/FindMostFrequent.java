ab423067183f0886ec9bb502241875e4a5d86741
sathishmepco@gmail.com <sathishmepco@gmail.com>
2020-07-16 23:38:18 +0530

    Find Most Frequent

--- InterviewPrograms/src/com/java/array/FindMostFrequent.java
+++ InterviewPrograms/src/com/java/array/FindMostFrequent.java
@@ -6,12 +6,12 @@ package com.java.array;
  * 
  * For the each element, scan the entire array 
  * to find its duplicates. Maintain two variables 
- * max_freq and ans to store the maximum frequent
- * and ans with that frequency respectively.
+ * max_freq and output to store the maximum frequent
+ * and output with that frequency respectively.
  *
  * Say array is {8, 9, 3, 3, 6, 3, 8, 1, 6};
  * start with 8 and scan all the element for 8 (i=0)
- * max_freq = 2 and ans = 8
+ * max_freq = 2 and output = 8
  * 
  * then next scan all the 9 (i=1)
  * 9 is repeated only one time so its count is = 1 
@@ -19,12 +19,12 @@ package com.java.array;
  * 
  * then next scan all the 3(i=2)
  * 3 is occurred 3 times in the array
- * now max_freq = 3 and ans = 3
+ * now max_freq = 3 and output = 3
  * 
  * Continue the same procedure for all the 
  * elements in the array
  * 
- * Finally ans variable will return the most 
+ * Finally output variable will return the most 
  * frequent element in the array.
  * 
  * When two elements have the same frequent values
@@ -33,29 +33,30 @@ package com.java.array;
  */
 public class FindMostFrequent {
 	public static void main(String[] args) {
-		int array[] = {3, 9, 1, 3, 6, 3, 8, 1, 6};
-//		int array[] = {1, 9, 1, 3, 2, 3, 10};
-//		int array[] = {2, 1, 2, 2, 1, 3};
+		int arr[] = {3, 9, 1, 3, 6, 3, 8, 1, 6};
+//		int arr[] = {1, 9, 1, 3, 2, 3, 10};
+//		int arr[] = {2, 1, 2, 2, 1, 3};
 
 		int max_freq = 0;
-		int ans = 0;
+		int output = 0;
+		int N = arr.length;
 		
-		for(int i=0;i<array.length;i++){
+		for(int i=0;i<N;i++){
 			int cur_freq = 1;
-			for(int j=i+1;j<array.length;j++){
-				if(array[i] == array[j])
+			for(int j=i+1;j<N;j++){
+				if(arr[i] == arr[j])
 					cur_freq++;
 			}
 			if(cur_freq > max_freq){
 				max_freq = cur_freq;
-				ans = array[i];
-			}else if(cur_freq == max_freq && ans > array[i]){
+				output = arr[i];
+			}else if(cur_freq == max_freq && output > arr[i]){
 				max_freq = cur_freq;
-				ans = array[i];
+				output = arr[i];
 			}
 		}
 		
-		System.out.println("Most Frequent element is :: "+ans);
+		System.out.println("Most Frequent element is :: "+output);
 		System.out.println("Occurred "+max_freq+" times.");
 	}
 }
