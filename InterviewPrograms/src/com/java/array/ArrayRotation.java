079eda9f00afc41abab0abc386f731a5eab47dcf
sathishmepco@gmail.com <sathishmepco@gmail.com>
2020-07-22 23:50:05 +0530

    Array Rotation

---
+++ InterviewPrograms/src/com/java/array/ArrayRotation.java
@@ -0,0 +1,25 @@
+package com.java.array;
+
+public class ArrayRotation {
+	public static void main(String[] args) {
+		int arr[] = {1, 2, 3, 4, 5, 6};
+		
+	}
+}
+/*
+	INPUT
+	arr[] = {1, 2, 3, 4, 5, 6}
+	d = 2
+	OUTPUT
+	{5, 6, 1, 2, 3, 4}
+	
+	
+	INPUT
+	arr[] = {1, 2, 3, 4, 5, 6}
+	d = 4
+	OUTPUT
+	{3, 4, 5, 6, 1, 2}
+	
+	INPUT
+	OUTPUT
+*/
