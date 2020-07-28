0b6e76825db9016fb03fdf6e02bde9adc0a6c4ba
sathishmepco@gmail.com <sathishmepco@gmail.com>
2020-07-28 23:17:56 +0530

    Transport of Matrix

--- InterviewPrograms/src/com/java/matrix/MatrixTranspose.java
+++ InterviewPrograms/src/com/java/matrix/MatrixTranspose.java
@@ -1,7 +1,5 @@
 package com.java.matrix;
 
-import java.util.Scanner;
-
 /*
  * Transpose of Matrix
  * It is simply a flipped version of original matrix.
@@ -19,65 +17,45 @@ import java.util.Scanner;
  */
 public class MatrixTranspose {
 	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-		System.out.println("Enter the no of rows in the matrix : ");
-		int rows = Integer.parseInt(scanner.nextLine().trim());
-		System.out.println("Enter the no of columns in the matrix : ");
-		int cols = Integer.parseInt(scanner.nextLine().trim());
+		int matrix[][] = {
+				{1,2,3},
+				{4,5,6}
+				};
+		int rows = 2;
+		int columns = 3;
 		
-		int matrix[][] = new int[rows][cols];
-		for(int i=0;i<rows;i++){
-			System.out.println("Enter the values (space separated values in one line) in the row :: "+(i+1));
-			String line = scanner.nextLine().trim();
-			String str[] = line.split("\\s");
-			for(int j=0;j<cols;j++){
-				matrix[i][j] = Integer.parseInt(str[j]);
-			}
+		int rowT = columns;
+		int colT = rows;
+		
+		int matrixT[][] = new int[rowT][colT];
+		for(int i=0;i<rowT;i++){
+			for(int j=0;j<colT;j++)
+				matrixT[i][j] = matrix[j][i];
 		}
 		
 		System.out.println("The given matrix is (say matrix A)::");
-		displayMatrix(rows, cols, matrix);
+		displayMatrix(matrix, rows, columns);
 		
-		int matrixT[][] = transposeMatrix(rows, cols, matrix);
 		System.out.println("Transpose of Given matrix is (say matrix AT)::");
-		displayMatrix(cols, rows, matrixT);
-		scanner.close();
+		displayMatrix(matrixT, rowT, colT);
 	}
 	
-	private static void displayMatrix(int rows,int cols, int matrix[][]){
+	private static void displayMatrix(int matrix[][],int rows,int cols){
 		for(int i=0;i<rows;i++){
 			for(int j=0;j<cols;j++)
 				System.out.print(matrix[i][j]+"\t");
 			System.out.println();
 		}
 	}
-	
-	private static int[][] transposeMatrix(int rows, int cols, int matrix[][]){
-		int matrixT[][] = new int[cols][rows];
-		for(int i=0;i<cols;i++){
-			for(int j=0;j<rows;j++)
-				matrixT[i][j] = matrix[j][i];
-		}
-		return matrixT;
-	}
 }
 
 /*
 	OUTPUT
-	
-	Enter the no of rows in the matrix : 
-	2
-	Enter the no of columns in the matrix : 
-	3
-	Enter the values (space separated values in one line) in the row :: 1
-	1 2 3
-	Enter the values (space separated values in one line) in the row :: 2
-	4 5 6
 	The given matrix is (say matrix A)::
 	1	2	3	
 	4	5	6	
 	Transpose of Given matrix is (say matrix AT)::
 	1	4	
 	2	5	
-	3	6
+	3	6	
 */
