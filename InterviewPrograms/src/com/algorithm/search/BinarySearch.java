aa6d53a918a3f9968777a3d4c5793e05b4be7d86
sathishmepco@gmail.com <sathishmepco@gmail.com>
2020-08-05 18:27:20 +0530

    Search Programs

--- InterviewPrograms/src/com/algorithm/search/BinarySearch.java
+++ InterviewPrograms/src/com/algorithm/search/BinarySearch.java
@@ -1,15 +1,36 @@
 package com.algorithm.search;
 
+/*
+ * Binary Search
+ * 
+ * In computer science, binary search, also known as 
+ * half-interval search is a search algorithm that 
+ * finds the position of a target value within a sorted array.
+ * 
+ * Binary search compares the target value to the middle element 
+ * of the array. If they are not equal, the half in which 
+ * the target cannot lie is eliminated and the search continues 
+ * on the remaining half, again taking the middle element 
+ * to compare to the target value, and repeating this until 
+ * the target value is found. If the search ends with the 
+ * remaining half being empty, the target is not in the array.
+ * 
+ * Time Complexity
+ * 
+ * Worst Case 	O(log n)
+ * Best Case	O(1)
+ * 
+ * This search is faster than linear search.
+ */
 public class BinarySearch {
-
 	public static void main(String[] args) {
 		int array[] = { 10, 20, 30, 40, 50 };
 		int key = 50;
 		int index = binarySearch(array, key);
 		if(index == -1)
-			System.out.println("Element not found.");
+			System.out.println("Element NOT found.");
 		else
-			System.out.println("Element found at "+index+" index");
+			System.out.println("Element found at index : "+index);
 	}
 
 	public static int binarySearch(int array[], int key) {
@@ -25,5 +46,15 @@ public class BinarySearch {
 		}
 		return -1;
 	}
-
 }
+/*
+	OUTPUT
+	array[] = { 10, 20, 30, 40, 50 }
+	key = 50
+	Element found at index : 4
+
+	OUTPUT
+	array[] = { 10, 20, 30, 40, 50 }
+	key = 100
+	Element NOT found.
+*/
--- InterviewPrograms/src/com/algorithm/search/LinearSearch.java
+++ InterviewPrograms/src/com/algorithm/search/LinearSearch.java
@@ -1,18 +1,43 @@
 package com.algorithm.search;
 
+/*
+ * Linear Search
+ * 
+ * In computer science, a linear search or 
+ * sequential search is a method for finding 
+ * an element within a list.
+ *  
+ * It sequentially checks each element of the 
+ * list until a match is found or the 
+ * whole list has been searched.
+ * 
+ * Time Complexity
+ * 
+ * Worst Case 	O(n)
+ * Best Case 	O(1)
+ */
 public class LinearSearch {
-	
 	public static void main(String[] args) {
 		int array[] ={3,60,35,2,45,320,5};
-		int X = 12;
+		int X = 320;
 		
 		for(int i=0;i<array.length;i++)
 			if(X == array[i]){
-				System.out.println("Element found!");
+				System.out.println("Element found at index : "+i);
 				return;
 			}
 			
-		System.out.println("Element not found");
+		System.out.println("Element NOT found");
 	}
-
 }
+/*
+	OUTPUT
+	array[] ={3,60,35,2,45,320,5}
+	X = 320
+	Element found at index : 5
+
+	OUTPUT
+	array[] ={3,60,35,2,45,320,5}
+	X = 12
+	Element NOT found
+*/
