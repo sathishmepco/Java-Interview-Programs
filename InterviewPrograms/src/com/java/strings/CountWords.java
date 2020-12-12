44921f2e67b2b41e7e5190636bf63e04f77b4722
sathishmepco@gmail.com <sathishmepco@gmail.com>
2020-12-13 00:50:40 +0530

    Count the words

--- InterviewPrograms/src/com/java/strings/CountWords.java
+++ InterviewPrograms/src/com/java/strings/CountWords.java
@@ -4,45 +4,50 @@ import java.util.Scanner;
 
 /*
  * Count the number of Words
- *
+ * --------------------------
  * Write the java program to count the number of
  * words in the given string without string functions
  * 
+ * solution::
+ * ----------
  * Count the number of white spaces " "
+ * 
+ * 
  * say Given String : java programming
  * no of words : 2
  * say Given String : code
  * no of words : 1
  *  
  */
+
 public class CountWords {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("Enter any String : ");
 		String str = scanner.nextLine().trim();
+		if(str.length() == 0){
+			System.out.println("Number of wrods : 0");
+			System.exit(0);
+		}
 		System.out.println("Number of words : "+countWords(str));
 		scanner.close();
 	}
 	
 	private static int countWords(String str){
-		int count = 0;
-		boolean isWord = false;
+		int count = 1;
 		for(int i=0;i<str.length();i++){
-			String ch = str.charAt(i)+"";
-			if(ch.equals(" ")){
+			char ch = str.charAt(i);
+			System.out.println(ch);
+			if(ch == ' ' || ch == '\t' || ch == '\n'){
 				count++;
-				isWord = false;
-				//this while loop removes continuous
-				//white spaces
-				while(ch.equals(" ")){
-					ch = str.charAt(i++)+"";
+				ch = str.charAt(i+1);
+				while(ch == ' ' || ch == '\t' || ch == '\n'){
+					i++;
+					ch = str.charAt(i+1);
+					//this while loop removes continuous white spaces
 				}
-			}else{
-				isWord = true;
 			}
 		}
-		if(isWord == true)
-			count++;
 		return count;
 	}
 }
@@ -51,12 +56,12 @@ public class CountWords {
 	Enter any String : java programming
 	Number of words : 2
 	
-	Enter any String : 123 123
+	Enter any String : 123		123
 	Number of words : 2
 		
 	Enter any String : code
 	Number of words : 1
 
-	Enter any String : I am a good coder
+	Enter any String : I am a good      coder
 	Number of words : 5
 */
